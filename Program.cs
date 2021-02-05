cfdbdc658b7f4b71f076987aa27818c05dc2baff
LC <llebpmacl1@gmail.com>
2021-02-05 16:29:36 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+    Author: Loni Campbell
+    Date: 2/5/20
+    Comment: Showing an auto populated array
+*/
+
+using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Here is my Auto-populated array!");
+            int total_elements = 26;
+            int[] countdown = new int[total_elements];
+            int element = 1;
+            for(int i = 1; i < total_elements; i++)
+            {
+                countdown[i] = element;
+                Console.WriteLine("Element Value: " + i);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
